9794d086bd0b6fb91de55ea285bfb6bef54af2ed
Dmitriy Molochkov <molochkov.dima@mail.ru>
2023-07-04 22:50:21 +0300

    Add pinoConfig selection depending on environment

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,15 +1,25 @@
 import pino from 'pino';
 
-const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-  },
-  formatters: {
-    level: (label) => {
-      return { level: label.toUpperCase() };
+import config from '#config';
+import { Environment } from '#constants';
+
+const pinoConfigsObj: Record<Environment, pino.LoggerOptions> = {
+  development: {
+    transport: {
+      target: 'pino-pretty',
     },
+    formatters: {
+      level: (label) => {
+        return { level: label.toUpperCase() };
+      },
+    },
+    timestamp: pino.stdTimeFunctions.isoTime,
   },
-  timestamp: pino.stdTimeFunctions.isoTime,
-});
+  production: {},
+};
+
+const pinoConfig = pinoConfigsObj[config.nodeEnv];
+
+const logger = pino(pinoConfig);
 
 export default logger;
