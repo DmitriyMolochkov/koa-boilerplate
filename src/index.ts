00f7f3d196dee31daf76d93ca79f654679497f3d
Dmitriy Molochkov <molochkov.dima@mail.ru>
2023-07-12 16:07:14 +0300

    Move 'start' function to main.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,6 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
-import { serverConfig } from '#config';
-import { DataSource } from '#database';
-import logger from '#logger';
-
-import { init } from './server';
-import onShutdown from './server/on-shutdown';
-
-const start = async () => {
-  try {
-    if (!DataSource.isInitialized) {
-      throw new Error('Database not initialized');
-    }
-
-    const koa = await init();
-    const server = koa.listen(
-      {
-        port: serverConfig.port,
-      },
-      async () => {
-        logger.info(`Server listening on port: ${serverConfig.port}`);
-        onShutdown(server);
-      },
-    );
-  } catch (err) {
-    logger.error(err, 'Error while starting the server');
-    process.exitCode = 1;
-  }
-};
+import start from './main';
 
 await start();
---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { serverConfig } from '#config';
+import { DataSource } from '#database';
+import logger from '#logger';
+
+import { init } from './server';
+import onShutdown from './server/on-shutdown';
+
+export default async function start() {
+  try {
+    if (!DataSource.isInitialized) {
+      throw new Error('Database not initialized');
+    }
+
+    const koa = await init();
+    const server = koa.listen(
+      {
+        port: serverConfig.port,
+      },
+      async () => {
+        logger.info(`Server listening on port: ${serverConfig.port}`);
+        onShutdown(server);
+      },
+    );
+  } catch (err) {
+    logger.error(err, 'Error while starting the server');
+    process.exitCode = 1;
+  }
+}
