a83db83c77cbec862c71c0d74e0bfa6261e00518
Dmitriy Molochkov <molochkov.dima@mail.ru>
2023-07-15 14:22:59 +0300

    Add uncaughtException, unhandledRejection event handlers

--- src/server/on-shutdown.ts
+++ src/server/on-shutdown.ts
@@ -1,25 +1,48 @@
 import { Server } from 'http';
+import * as util from 'util';
 
 import { DataSource } from '#database';
 import logger from '#logger';
 
 const signals = ['SIGTERM', 'SIGINT', 'SIGUSR2'];
 
+async function shutdownServer(server: Server) {
+  try {
+    await util.promisify(server.close.bind(server))();
+
+    if (DataSource.isInitialized) {
+      await DataSource.destroy();
+    }
+  } catch (error) {
+    logger.error(error, 'Error while shutdown the server');
+  }
+}
+
 export default function onShutdown(server: Server) {
   const handleSignal = async (signal: string) => {
     logger.info(`Received shutdown signal: ${signal}`);
     signals.forEach((sig) => process.removeListener(sig, handleSignal));
 
-    await new Promise((resolve) => {
-      server.close(resolve);
-    });
-
-    if (DataSource.isInitialized) {
-      await DataSource.destroy();
-    }
+    await shutdownServer(server);
 
     process.kill(process.pid, signal);
   };
 
   signals.forEach((signal: string) => process.on(signal, handleSignal));
+
+  process.on('uncaughtException', async (error) => {
+    logger.error(error, 'Uncaught exception');
+
+    await shutdownServer(server);
+
+    process.exit(1);
+  });
+
+  process.on('unhandledRejection', async (error) => {
+    logger.error(error, 'Unhandled promise rejection');
+
+    await shutdownServer(server);
+
+    process.exit(1);
+  });
 }
